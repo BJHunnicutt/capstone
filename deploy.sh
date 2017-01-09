e767dc8163b23d0767bb3d9fc2ef8b9a7566e414
“BJHunnicutt” <jeannie.hunnicutt@gmail.com>
2017-01-09 14:56:07 -0800

    added d3

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ git branch -D gh-pages
 # Recreate it
 git checkout -B gh-pages
 
-# Build the new project for production, based on : https://github.com/facebookincubator/create-react-app/blob/dcdcab0ecc86d9643bc9c3c9a9eee8bbad63bb43/template/README.md#github-pages
+# Build the new project for production, based on :
+# https://github.com/facebookincubator/create-react-app/blob/dcdcab0ecc86d9643bc9c3c9a9eee8bbad63bb43/template/README.md#github-pages
+# & https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#deployment
+
 npm run build
 
 git add -f build
